87ab2fe9a01e847ef87e8fb1c96ca09d2be98084
Fran Moreno <franmomu@gmail.com>
2013-04-28 18:30:24 +0200

    Added id to the findAll query in UserManager

--- src/SMTC/Silex/Service/UserManager.php
+++ src/SMTC/Silex/Service/UserManager.php
@@ -14,7 +14,7 @@ class UserManager
     public function findAll()
     {
         return $this->userFactory
-            ->select_many_expr('firstName', 'lastName', array('addresses' => "group_concat(a.street, '|')"))
+            ->select_many_expr('u.id','u.firstName', 'u.lastName', array('addresses' => "group_concat(a.street, '|')"))
             ->table_alias('u')
             ->join('address', array('u.id', '=', 'a.user_id'), 'a')
             ->group_by('u.id')
